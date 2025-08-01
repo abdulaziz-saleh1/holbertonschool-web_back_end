d0544c474dc5eb16e33c3a44d8b004c087dd215d
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-08-01 17:49:01 +0000

    to Kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module provides a function that returns a tuple of a string and the square of a number."""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple where the first element is a string and the second is the square of a number."""
+    return (k, float(v ** 2))
