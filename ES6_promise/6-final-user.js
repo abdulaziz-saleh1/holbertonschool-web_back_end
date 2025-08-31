ed1d2618b4d86f901d8c93544b3d6d76f1c362ef
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-08-31 19:29:03 +0000

    signUpUser

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((res) =>
+      res.status === 'fulfilled'
+        ? { status: res.status, value: res.value }
+        : { status: res.status, value: String(res.reason) }
+    )
+  );
 }
